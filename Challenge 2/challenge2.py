403221f77e26f481e621e705b3b5a6312df3970d
Taran Knowles <tknowles2013@fau.edu>
2018-03-16 16:09:30 -0400

    Update challenge2.py

--- Challenge 2/challenge2.py
+++ Challenge 2/challenge2.py
@@ -1,40 +1,97 @@
+# Taran Knowles
+# MotionPoint - Technical Analyst Assessment
+# Challenge 2
+# KNOWLES_TECH_SOLUTIONS
+
 from requests import get
 from bs4 import BeautifulSoup
 import csv
 import time
 
-with open('challenger2.csv', 'rt') as f:
-    data = list(csv.reader(f))
+# function to scrape the title and meta description, given the url
+# outputs title, description, url name, and length of title+description to CSV
+def scrape(url):
 
-writer = csv.writer(open(r'C:\Users\Rinzler\PycharmProjects\challenge2\challenger2.csv', 'a'))
+    # initialize writer to append data to CSV file
+    # if viewing CSV in excel, close it out at the time of program run to avoid throwing errors
+    writer = csv.writer(open(r'uniqueFileName.csv', 'a'))
 
+    # grab URL
+    response = get(url)
+
+    # initialize BeautifulSoup parser with URL
+    htmlSoup = BeautifulSoup(response.text, 'html.parser')
+
+    # parse and write the title and description to variables (lists)
+    writeTitle = htmlSoup.title.contents
+    writeMeta = htmlSoup.findAll(attrs={"name": "description"})
+
+    # convert title and description to string to remove fodder and calculate length
+    writeTitle = str(writeTitle)
+    writeMeta = str(writeMeta)
+
+    # remove fodder from beginning and end of title and description
+    writeTitle = writeTitle[:-2]
+    writeMeta = writeMeta[:-23]
+    writeTitle = writeTitle[2:]
+    writeMeta = writeMeta[16:]
+
+    # print title and description to console
+    print(writeTitle)
+    print(writeMeta)
+
+    # pull length of title and description
+    lenTitle = len(str(writeTitle))
+    lenMeta = len(str(writeMeta))
+
+    # find total length of title and description
+    lenTotal = lenTitle + lenMeta
+
+    # write data to CSV, underneath headers
+    writer.writerow([writeTitle, writeMeta, url, lenTotal])
+
+# Start at index/landing page
+linkGet = get('https://www.motionpoint.com/')
+
+# initialize BeautifulSoup parser with URL
+links_soup = BeautifulSoup(linkGet.text, 'html.parser')
+
+# create a CSV file to store data
+with open('uniqueFileName.csv', 'wb') as csvfile:
+
+    # initialize header to overwrite previous data in CSV file
+    header = csv.writer(open(r'uniqueFileName.csv', 'w'))
+
+    # write column headers to CSV, under which scraped data will be stored
+    header.writerow(['Title', 'Description', 'URL', 'Length'])
+
+# create list of all hrefs found in index/landing page
+linkList = []
+for link in links_soup.findAll('a'):
+    linkList.append(link.get('href'))
+
+# create new list, that of which will store URLs from linkList that are greater than 28 characters
+# this is to remove URLs such as https://www.motionpoint.com and https://es.motionpoint.com
+long_links = []
+for link in linkList:
+    length = len(link)
+    if length > 28:
+        long_links.append(link)
+
+# assign array items to variables to be passed into functions
+mainLink = linkList[0]
+secondLink = long_links[0]
+thirdLink = long_links[1]
+fourthLink = long_links[2]
+fifthLink = long_links[3]
+
+# run through scraping functions for each passed url, with a 5 second delay in between
+scrape(mainLink)
 time.sleep(5)
-
-url = 'https://www.motionpoint.com/'
-response = get(url)
-
-html_soup = BeautifulSoup(response.text, 'html.parser')
-print(html_soup.title)
-
-for tags in html_soup.find_all('meta'):
-    print(tags.get('content'))
-
-write_title = html_soup.title
-# write_link = html_soup('href')
-write_meta = html_soup.find_all('meta')
-
-writeArray = [write_title, write_meta]
-
-lenTitle = len(str(write_title))
-lenMeta = len(str(write_meta))
-
-lenTotal = lenTitle + lenMeta
-
-printy = sum(len(i) for i in writeArray)
-
-length = len(write_title)
-# length1 = len(write_link)
-length2 = len(write_meta)
-full_length = length + length2
-
-writer.writerow([write_title, write_meta, url, lenTotal])
+scrape(secondLink)
+time.sleep(5)
+scrape(thirdLink)
+time.sleep(5)
+scrape(fourthLink)
+time.sleep(5)
+scrape(fifthLink)
